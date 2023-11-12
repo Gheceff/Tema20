9c127a53a3a25b8dde7ee1a06a82c0ddcacd004d
Gheceff <91993800+Gheceff@users.noreply.github.com>
2023-11-12 15:01:29 +0200

    Add files via upload

---
+++ Tema20_Gheceff_Eduard/scripts.js
@@ -0,0 +1,69 @@
+/* Valori de tip String */
+
+/* Exercitiul 1*/
+
+var element1 = "Text1"
+var element2 = "Text2"
+var element3 ="Text3"
+var element1 = element1.concat(" ", "+",);
+console.log( element1 ,element2, "+",element3)
+console.log(element1+element2 ,""+"+",element3)
+
+
+/*Exercitiul 2*/
+
+myString = '200' 
+console.log(parseInt(myString)) // Rezultat: 200
+
+/*Exercitiul 3*/
+
+let text = "Eduard.Gheceff"
+let length = text.length
+console.log("Eduard.Gheceff =",length)
+
+/* Exercitiul 4*/
+
+let textul = "We are \"Vikings\" ";
+console.log(textul)
+
+
+/* Valori de tip Array */
+
+/* Exercitiul 1*/
+
+var transport = ["Masina", "Bicicleta", "Trotineta"];
+console.log("Vector initial:", transport);
+console.log("Primul element:", transport[0])
+
+/*Exercitiul 2 */
+console.log("Ultimul element:",transport[2])
+
+
+/* Exercitiul 3 */
+var cars = [10, 30, 12, 40,5]
+console.log("Numere ",cars)
+console.log("Suma primelor 2 numere :",10 + 30)
+console.log("Produsul ultimelor 3 :",12*40*5)
+console.log("Media artimetica :",40/2 + 2400/3)
+
+/* Exercitiul 4*/
+arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+arr1.sort()
+console.log("Sort : ",arr1)
+
+/* Exercitiul 5*/
+
+myColor = ["Red", "Green", "White", "Black"]
+console.log("Combinate : ",myColor.join())
+
+/* Exercitiul 6*/
+var vector1 = [1,2,3]
+var vector2 = ["Avion", "Motor"]
+var vector = [vector1 + vector2]
+console.log("Vector unit :",vector)
+console.log("Lungimea : ",vector1.length + vector2.length)
+console.log("Lungimea vector1 :", vector1.length)
+
+
+
+
